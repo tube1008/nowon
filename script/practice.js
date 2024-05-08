235a0f52f9d9e2601ba783198355b271e7c21662
tube1008 <ilhyuk0504@gmail.com>
2024-05-08 18:01:59 +0900

    스크롤바 수정

--- script/practice.js
+++ script/practice.js
@@ -1,11 +1,11 @@
-const popup = document.querySelector ('.popup')
-const bg = document.querySelector ('.bg')
-const click = document.querySelector('.click')
-console.log(popup, bg, click)
+// const popup = document.querySelector ('.popup')
+// const bg = document.querySelector ('.bg')
+// const click = document.querySelector('.click')
+// console.log(popup, bg, click)
 
-popup.style.display = 'none'
+// popup.style.display = 'none'
 
-click.addEventListener('click', function (e) { 
-    popup.style.display = 'block';
-    e.preventDefault();
-})
+// click.addEventListener('click', function (e) {
+//     popup.style.display = 'block';
+//     e.preventDefault();
+// })
